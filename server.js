8c449ffec4ed4882da4791e2e5dcca0f7462382b
NoName15 <43522880+NoName15@users.noreply.github.com>
2018-11-22 21:55:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,32 +7,32 @@
 //status
 const devs1 = ['480407581085532180'];
 var prefix = "!";
-client.on('message', message => {
+bot.on('message', message => {
     let argresult = message.content.split(` `).slice(1).join(' ');
     if (message.content.startsWith(prefix + 'setStreaming')) {
       if (!devs1.includes(message.author.id)) return message.channel.send("<@480407581085532180> only this guy can do restart the bot so don't try again :wink:.");
       message.delete();
-      client.user.setGame(argresult, 'https://twitch.tv/DynastyShop');
+      bot.user.setGame(argresult, 'https://twitch.tv/DynastyShop');
 
     } else if(message.content.startsWith(prefix + 'setWatching')) {
-        client.user.setActivity(argresult,{type: 'WATCHING'});
+        bot.user.setActivity(argresult,{type: 'WATCHING'});
 
       } else if(message.content.startsWith(prefix + 'setListening')) {
-        client.user.setActivity(argresult,{type: 'LISTENING'});
+        bot.user.setActivity(argresult,{type: 'LISTENING'});
 
       } else if(message.content.startsWith(prefix + 'setPlaying')) {
-        client.user.setActivity(argresult,{type: 'PLAYING'});
+        bot.user.setActivity(argresult,{type: 'PLAYING'});
 
       } else if(message.content.startsWith(prefix + 'setName')) {
-        client.user.setUsername(argresult);
+        bot.user.setUsername(argresult);
 
       } else if(message.content.startsWith(prefix + 'setAvatar')) {
-        client.user.setAvatar(argresult);
+        bot.user.setAvatar(argresult);
 
 
       } else if(message.content.startsWith(prefix + 'setStatus')) {
         if(!argresult) return message.channel.send('`online`, `DND(Do not Distrub),` `idle`, `invisible(Offline)` :notes: أختر أحد الحالات');
-        client.user.setStatus(argresult);
+        bot.user.setStatus(argresult);
 
 
     }
